23a292b47c2deca9879ee1566ac062020040b55f
necobra <necobrane105@gmail.com>
2023-09-27 18:46:46 +0300

    lab01

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,46 @@
-const testModules = require('./test-module');
-require('../css/app.css');
+// const testModules = require('./test-module');
+// require('../css/app.css');
 
-/** ******** Your code here! *********** */
+// console.log(testModules.hello);
 
-console.log(testModules.hello);
+const openPopupButtons = document.querySelectorAll('.add-teacher-btn');
+console.log(openPopupButtons);
+const popupContainer = document.getElementById('popup-add-teacher');
+const closePopupButton = document.getElementById('closePopupAddTeacher');
+
+openPopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        popupContainer.style.display = 'flex';
+    });
+});
+
+closePopupButton.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+});
+
+popupContainer.addEventListener('click', (event) => {
+    if (event.target === popupContainer) {
+        popupContainer.style.display = 'none';
+    }
+});
+
+
+const openPopupDescriptionButtons = document.querySelectorAll('.teacher-card');
+const closePopupButtonDesc = document.getElementById('closePopup');
+const popupDescriptionContainer = document.getElementById('popup-description');
+
+openPopupDescriptionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        popupDescriptionContainer.style.display = 'flex';
+    });
+});
+
+closePopupButtonDesc.addEventListener('click', () => {
+    popupDescriptionContainer.style.display = 'none';
+});
+
+popupDescriptionContainer.addEventListener('click', (event) => {
+    if (event.target === popupDescriptionContainer) {
+        popupDescriptionContainer.style.display = 'none';
+    }
+});
